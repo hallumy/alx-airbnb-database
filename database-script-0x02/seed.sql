4227cd8670e1b8e174c30237abedb0df1d4c1787
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-09-06 11:22:29 +0300

    Add seed.sql

---
+++ database-script-0x02/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users (user_id, first_name, last_name, email, password_hash, role, created_at) VALUES
+    (uuid(), 'John', 'Doe', 'johndoe@example.com', 'password', '1234567890', 'guest') 
